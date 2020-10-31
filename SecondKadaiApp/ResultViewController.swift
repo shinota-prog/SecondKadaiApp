a671d33a483d8fd54a3b317e461f048630d5d3df
shin.ota <shin.ota0817@gmail.com>
2020-10-31 15:58:43 +0900

    change

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by Shin Ota on 10/27/20.
+//  Copyright © 2020 Shin Ota. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var name : String = ""
+    
+    @IBOutlet weak var outPutLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let outPutValue  = name
+        outPutLabel.text = "こんにちは、\(outPutValue) さん"
+    }
+    
+
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var inputTable: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    // segueから遷移先のResultViewControllerを取得する
+   
+        
+    let resultViewController : ResultViewController = segue.destination as! ResultViewController
+        
+        resultViewController.name = inputTable.text!
+        
     }
-
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
 
 }
 
